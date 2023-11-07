65f1fcca6805ba808ed206ddc7a33a86143e64ae
Sergey Mazilin <mazilin.sergey@yandex.ru>
2023-11-08 02:48:26 +0400

    settings.py

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -85,11 +85,11 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 # }
 
 DATABASES = {
-     'default': {
+        'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB', 'django'),
         'USER': os.getenv('POSTGRES_USER', 'django'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', ''),
         'PORT': os.getenv('DB_PORT'),
     }
