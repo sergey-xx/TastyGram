1c2e9605bdbd445e9a435de8ba887b454207e782
Sergey Mazilin <mazilin.sergey@yandex.ru>
2023-11-08 19:18:32 +0400

    pep8

--- backend/api/views.py
+++ backend/api/views.py
@@ -10,9 +10,6 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.views import APIView
 from rest_framework import serializers
-from rest_framework.pagination import (LimitOffsetPagination,
-                                       PageNumberPagination)
-
 
 from recipes.models import (Tag, Recipe, RecipeIngredient, Favorite, Follow,
                             Ingredient, ShoppingCart)
